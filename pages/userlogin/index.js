e028ac0f1c270ba53237a360bdc47a676874567f
18595451728 <rongyuejiaoyu.com>
2019-10-24 09:16:08 +0800

    newbd01

--- pages/userlogin/index.js
+++ pages/userlogin/index.js
@@ -16,6 +16,7 @@ Page({
     roomlist:[],
     community_id: '',
     community_build_id:'',
+    community_room_id: '',
     consignee: '',
     community: '',
 
@@ -90,7 +91,8 @@ Page({
       showxiaoqu: true,
       community: this.data.communitylist[e.currentTarget.dataset.index].class_name,
       community_id: this.data.communitylist[e.currentTarget.dataset.index].community_id,
-      community_build: ''
+      community_build: '',
+      community_room: ''
     })
     // console.log(this.data.communitylist[e.currentTarget.dataset.index]);
   },
@@ -100,14 +102,15 @@ Page({
       showdong: true,
       community_build: this.data.donglist[e.currentTarget.dataset.index].class_name,
       community_build_id: this.data.donglist[e.currentTarget.dataset.index].community_build_id,
-      // community_build: ''
+     community_room: ''
     })
   },
 
   roomchoose:function(e){
     this.setData({
       showroom: true,
-      community_room: this.data.roomlist[e.currentTarget.dataset.index].class_name
+      community_room: this.data.roomlist[e.currentTarget.dataset.index].class_name,
+      community_room_id: this.data.roomlist[e.currentTarget.dataset.index].community_room_id,
     })
   },
 
@@ -115,77 +118,98 @@ Page({
     console.log(e)
     var that = this
 
-    // var community = e.detail.value.community;
-    // var city = that.data.city_id;
+    var community = that.data.community;
+    var community_build = that.data.community_build;
+    var community_room = that.data.community_room;
+    
+    var consignee = e.detail.value.consignee;
+    var telephone = e.detail.value.telephone;
+    var password = e.detail.value.password;
+    
+    if (community == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请选择小区',
+        showCancel: false
+      })
+      return
+    }
+    if (community_build == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请选择幢',
+        showCancel: false
+      })
+      return
+    }
+    
+    if (community_room == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请选择门牌',
+        showCancel: false
+      })
+      return
+    }
 
-    // var consignee = e.detail.value.consignee;
-    // var telephone = e.detail.value.telephone;
+    if (consignee == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请填写联系人姓名',
+        showCancel: false
+      })
+      return
+    }
 
-    // if (community == "") {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: '请填写地区',
-    //     showCancel: false
-    //   })
-    //   return
-    // }
+    if (telephone == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请填写手机号码',
+        showCancel: false
+      })
+      return
+    }
 
+    if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(telephone))) {
+      wx.showModal({
+        title: '提示',
+        content: '手机号格式错误',
+        showCancel: false
+      })
+      return
+    }
 
+    if (password == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请选择密码',
+        showCancel: false
+      })
+      return
+    }
 
-    // if (consignee == "") {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: '请填写联系人姓名',
-    //     showCancel: false
-    //   })
-    //   return
-    // }
-    // if (telephone == "") {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: '请填写手机号码',
-    //     showCancel: false
-    //   })
-    //   return
-    // }
-
-    // if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(telephone))) {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: '手机号格式错误',
-    //     showCancel: false
-    //   })
-    //   return
-    // }
    
-
-    // if (address == "") {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: '请填写详细地址',
-    //     showCancel: false
-    //   })
-    //   return
-    // }
-
     r.req(u + '/api/user/bind', {
-      community_id: that.data.communitylist,
-      community_build_id: that.data.community_build,
-      community_room_id: that.data.community_room,
+      community_id: that.data.community_id,
+      community_build_id: that.data.community_build_id,
+      community_room_id: that.data.community_room_id,
       name: e.detail.value.consignee,
       tel: e.detail.value.telephone,
       password: e.detail.value.password,
       token: wx.getStorageSync('token')
     }, 'post').then((res) => {
       console.log(res)
-      wx.showModal({
-        title: '提示',
-        content: '保存成功',
-        showCancel: false
-      })
-        wx.navigateTo({
-          url: '/pages/index/index',
-        })
+      // that.setData({
+        // wx.showModal({
+        //   title: '提示',
+        //   content: '保存成功',
+        //   showCancel: false
+        // })
+      // })
+     
+        // wx.navigateTo({
+        //   url: '/pages/index/index',
+        // })
     }, 500)
 
 
